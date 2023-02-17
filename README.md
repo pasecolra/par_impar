94e329bc46468eea069864ab77c476a39ab4c2c0
felipepedraza22 <carlospedraza@colegioguanenta.edu.co>
2023-02-17 07:45:51 -0500

    se agrega los programas

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # par_impar
 programa para diferenciar los números pares y impares
+
+# diagrama de flujo
+
+![diagrama de flujo](diagrama.png "diagrama de flujo")
---
+++ par_impar.py
@@ -0,0 +1,9 @@
+X = int(input("se ingresa un numero: "))
+
+R = X%2
+
+if R==0:
+    msj="par"
+else:
+    msj="impar"
+print("el numero " + str(X) + " es: " + msj)
